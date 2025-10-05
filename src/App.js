5b8c7014def66a6800f3b32f7c0884904dfe9059
12001300 <firasneme095@gmail.com>
2025-10-05 19:56:18 +0300

    this commit for adding the navbar

--- src/App.js
+++ src/App.js
@@ -1,34 +1,20 @@
 import React from 'react'
-import Button from '@mui/material/Button';
-import DeleteIcone from '@mui/icons-material/Delete';
-import Add from '@mui/icons-material/Add';
-import Typography from '@mui/material/Typography';
-import { styled } from '@mui/material/styles';
+import SideBar from './Components/SideBar'
+import Feed from './Components/Feed'
+import Rightbar from './Components/Rightbar'
+import { Box, Container, Stack } from '@mui/material'
+import Navbar from './Components/Navbar'
+
 export default function App() {
-  const  Bluebutton=styled(Button)({
-     backgroundColor:"skyblue",
-  color:"#888",
-  margin:5,
-  "&:hover":{backgroundColor:"red"},
-  "&:disabled":{backgroundColor:"black",color:"white"
-  }})
   return (
-    <div>
-      <Button startIcon={<Add/>} variant="outlined" color='success'>Text</Button>
-<Button variant="contained" color='secondary' startIcon={<DeleteIcone/>}>Contained</Button>
-<Button variant="outlined" disabled>Outlined</Button>
-<Typography variant='h6' component="p">
-  it uses h1  style 
-</Typography>
-<Button variant='contained' disabled  sx={{
-  backgroundColor:"skyblue",
-  color:"#888",
-  margin:5,
-  "&:hover":{backgroundColor:"red"},
-  "&:disabled":{backgroundColor:"grey",color:"white"}
-}}> my button</Button>
-<Bluebutton disabled
-> unique button</Bluebutton>
-    </div>
+    <Box >
+      <Navbar />
+      <Stack direction={"row" } spacing={2} justifyContent="center" >
+
+      <SideBar />
+      <Feed />
+      <Rightbar />
+      </Stack>
+    </Box>
   )
 }
---
+++ src/Components/SideBar.js
@@ -0,0 +1,11 @@
+import { Box } from '@mui/material'
+import React from 'react'
+
+export default function SideBar() {
+  return (
+    <Box 
+      bgcolor={"lightgreen"} flex={1} p={2} sx={{display:{xs:"none", sm:"block"}}}>
+      SideBar
+    </Box>
+  )
+}
